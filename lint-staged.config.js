a2dcf2221463bc3205f92d495ac2f1e52d285514
evilebottnawi <sheo13666q@gmail.com>
2019-03-12 17:17:47 +0300

    chore(deps): update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,15 +1,18 @@
 "use strict";
 
 module.exports = {
-  "*.{js,mjs,jsx}": [
-    "prettier --list-different",
-    "eslint --report-unused-disable-directives",
-    "git add"
-  ],
-  "*.{md,markdown,mdown,mkdn,mkd,mdwn,mkdown,ron}": [
-    "prettier --list-different",
-    "remark -f -q",
-    "git add"
-  ],
-  "package.json": ["npmPkgJsonLint -q", "git add"]
+  ignore: ["package-lock.json"],
+  linters: {
+    "*.{js,mjs,jsx}": [
+      "prettier --list-different",
+      "eslint --report-unused-disable-directives",
+      "git add"
+    ],
+    "*.{md,markdown,mdown,mkdn,mkd,mdwn,mkdown,ron}": [
+      "prettier --list-different",
+      "remark -f -q",
+      "git add"
+    ],
+    "package.json": ["npmPkgJsonLint -q", "git add"]
+  }
 };
